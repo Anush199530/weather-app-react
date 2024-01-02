2d2ac3a3b74dae4fe76aaf1cfc7d7ff047a39b13
Anush199530 <Anush199530@users.noreply.github.com>
2024-01-02 11:14:27 +0100

    added the github url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This probject is coded by Anush Arutunian and is{" "}
+        <a
+          href="https://github.com/Anush199530/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
