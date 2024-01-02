32846d04e3032e80629d0e511315e85dc177263f
Anush199530 <Anush199530@users.noreply.github.com>
2024-01-02 11:54:20 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This probject is coded by Anush Arutunian and is{" "}
-        <a
-          href="https://github.com/Anush199530/weather-app-react"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+    <div className="App container">
+      <div className="container">
+        <h1> Weather App</h1>
+        
+        <footer>
+          This probject is coded by Anush Arutunian and is{" "}
+          <a
+            href="https://github.com/Anush199530/weather-app-react"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
